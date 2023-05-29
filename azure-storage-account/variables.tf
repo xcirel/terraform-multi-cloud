959f0ce887cd27a60bf4b4c12e3c4cf5ccf91d65
Eric Santos <eric.santos@newrizon.global>
2023-05-29 14:52:41 -0300

    Working with variables

---
+++ azure-storage-account/variables.tf
@@ -0,0 +1,17 @@
+variable "location" {
+  description = "Váriável que indica a região onde os recursos serão criados"
+  type = string
+  default = "East US"
+}
+
+variable "account_tier" {
+  description = "Tier da Storage Account na Azure"
+  type = string
+  default = "Standard"
+}
+
+variable "account_replication_type" {
+  description = "Tipo de replicação de dados da Storage Account"
+  type = string
+  default = "LRS"
+}
