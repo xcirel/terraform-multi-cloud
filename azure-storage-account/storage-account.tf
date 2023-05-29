c280c352a20bc743f8c739e2f90ed79849f1ddcc
Eric Santos <eric.santos@newrizon.global>
2023-05-29 15:15:45 -0300

    Working with reference attributes of other blocks

--- azure-storage-account/storage-account.tf
+++ azure-storage-account/storage-account.tf
@@ -1,16 +1,16 @@
 resource "azurerm_resource_group" "first_resource_group_iac" {
-  name     = ""
-  location = ""
+  name     = "storage_account_resource_group"
+  location = var.location
 
   tags = ""
 }
 
 resource "azurerm_storage_account" "first_storage_account_iac" {
-  name                     = ""
-  resource_group_name      = ""
-  location                 = ""
-  account_tier             = ""
-  account_replication_type = ""
+  name                     = "ericsantosstorageaccount"
+  resource_group_name      = azurerm_resource_group.first_resource_group_iac.name
+  location                 = var.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
 
   tags = {
     environment = ""
@@ -19,5 +19,5 @@ resource "azurerm_storage_account" "first_storage_account_iac" {
 
 resource "azurerm_storage_container" "first_container_iac" {
   name                  = ""
-  storage_account_name  = ""
+  storage_account_name  = azurerm_storage_account.first_storage_account_iac.name
 }
