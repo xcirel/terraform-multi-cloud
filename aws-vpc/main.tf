5b43a21bb73beda80a2e8283cf5df67065df4ebd
Eric Santos <eric.santos@newrizon.global>
2023-05-31 14:37:35 -0300

    Creating a complete VPC

---
+++ aws-vpc/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">=1.0.0"
+
+  required_providers {
+    aws = {
+      version = "3.73.0"
+      source  = "hashicorp/aws"
+    }
+  }
+
+  backend "s3" {
+    bucket = "ericsantos-remote-state"
+    key    = "aws-vpc/terraform.tfstate"
+    region = "us-east-1"
+  }
+
+
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      owner      = "ericsantos"
+      managed-by = "terraform"
+    }
+  }
+}
