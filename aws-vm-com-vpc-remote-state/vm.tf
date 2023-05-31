ecfd216562fe7929c2f80ba670f34d695639f3fb
Eric Santos <eric.santos@newrizon.global>
2023-05-31 17:45:45 -0300

    Creating VM in AWS

--- aws-vm-com-vpc-remote-state/vm.tf
+++ aws-vm-com-vpc-remote-state/vm.tf
@@ -1,4 +1,17 @@
 resource "aws_key_pair" "key" {
   key_name   = "aws-key-terraform"
-  public_key = file("~/aws-key-terraform")
+  public_key = file("~/aws-key-terraform.pub")
+}
+
+resource "aws_instance" "vm" {
+  ami                         = "ami-053b0d53c279acc90" # Ubuntu 22.04 LTS
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.key.key_name
+  subnet_id                   = data.terraform_remote_state.vpc.outputs.subnet_id
+  vpc_security_group_ids      = [data.terraform_remote_state.vpc.outputs.security_group_id]
+  associate_public_ip_address = true
+
+  tags = {
+    "Name" = "vm-terraform"
+  }
 }
