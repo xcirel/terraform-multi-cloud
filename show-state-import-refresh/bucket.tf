8ee12ff22a09e5e29f85c0a3e03aa1889ded4ff4
Eric Santos <eric@xcirel.net>
2024-04-08 19:16:26 -0300

    Working with show and stage command

---
+++ show-state-import-refresh/bucket.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "bucket_um" {
+  bucket = "xcirel-lab-terraform-commands-um"
+}
+
+resource "aws_s3_bucket" "bucket_2" {
+  bucket = "xcirel-lab-terraform-commands-2"
+}
+
+resource "aws_s3_bucket" "bucket_3" {
+  bucket = "xcirel-lab-terraform-commands-3"
+}
